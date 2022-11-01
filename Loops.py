620b1b960a247545b17d8bdea7c3733eb74ab012
Sureyya Akis <sureyyaabetul@gmail.com>
2022-11-01 09:40:50 -0700

    Create Loops.py

---
+++ Loops.py
@@ -0,0 +1,11 @@
+for number in range(10):
+  print(number)
+
+fav_movies = ["Dune", "Sandlot", "Godfather"]
+
+for movies in fav_movies:
+  print(movies)
+
+
+for num in range(40):
+  print((num+1)*2)
