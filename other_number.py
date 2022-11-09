e8b5a1a3dc8cd2c7749e311dd67beda1309c7612
Sureyya Akis <sureyyaabetul@gmail.com>
2022-11-09 12:23:09 -0800

    Create other_number.py

---
+++ other_number.py
@@ -0,0 +1,21 @@
+from decimal import Decimal, getcontext
+
+## Integers
+
+int('100')
+int('100', 2)
+int(100, 2)
+int('1ab', 16)
+1.2 - 1.0
+
+## Decimals
+
+getcontext()
+getcontext().prec=4
+getcontext()
+Decimal(1) / Decimal(3)
+getcontext().prec=2
+Decimal(1) / Decimal(3)
+Decimal(3.14)
+Decimal('3.14')
+round(1.2 - 1.0, 2)
