f9d822a090a02b51cac6cae901b6e22f158cfba8
Sureyya Akis <sureyyaabetul@gmail.com>
2022-11-09 13:14:58 -0800

    Create tuples_sets.py

---
+++ tuples_sets.py
@@ -0,0 +1,62 @@
+## Sets
+
+mySet = {'a', 'b', 'c'}
+mySet
+
+mySet = set(('a', 'b', 'c'))
+
+mySet
+
+myList = ['a', 'b', 'b', 'c', 'c']
+myList = list(set(myList))
+myList
+
+mySet[0]
+
+number = 1
+1[0]
+
+mySet.add('d')
+mySet
+
+'a' in mySet
+
+'z' in mySet
+
+len(mySet)
+
+while len(mySet):
+    print(mySet.pop())
+
+mySet
+
+mySet = {'a', 'b', 'c'}
+
+mySet.discard('a')
+
+mySet
+
+## Tuples
+
+myTuple = ('a', 'b', 'c')
+myTuple
+
+myTuple[0]
+
+myTuple[0] = 'd'
+
+def returnsMultipleValues():
+    return 1,2,3
+
+type(returnsMultipleValues())
+
+myTuple = (1,2,3)
+
+type(myTuple)
+
+a, b, c = returnsMultipleValues()
+
+print(a)
+print(b)
+print(c)
+
